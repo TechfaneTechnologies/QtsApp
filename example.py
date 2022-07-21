2e1ddd8a869e7381e75d58e955061199512ba724
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-07-21 14:18:16 +0530

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from qtsapp import *
 # logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    # if all greeks icluding 3rd order greeks is required to be streaming in terminal, use QtsAppRun(_stream=True) at amrket time
+    # Note: if all greeks icluding 3rd order greeks is required to be streaming in terminal, use QtsAppRun(_stream=True) at amrket time
     # QtsAppRun(_stream=True)
-    # Else Use QtsAppRun() or QtsAppRun(_stream=False) to get the option chain update din the excel sheet every 7-9 secs.
+    # Note: Else Use QtsAppRun() or QtsAppRun(_stream=False) to get the option chain update din the excel sheet every 7-9 secs.
     QtsAppRun()
