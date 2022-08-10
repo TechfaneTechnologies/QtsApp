4d9bbd0e25b73f8c5a333d6823368ac59d2257c9
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-08-10 20:16:43 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,16 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon August 8, 08:09:56 2022
+@author: DrJuneMoone
+"""
+
 import logging
 from qtsapp import *
+
+##############################################################################
+# If You Need Logging, uncomment The BeloW Line
 # logging.basicConfig(level=logging.DEBUG)
 
+global qtsapp, stream, _is_running_live, first_connect
+_is_running_live = False
+first_connect = True
+
+##############################################################################
+# If Not Using Ms.Office 365
+wb_name = "OptionChain"
+
+# If You Are Using Ms.Office 365 Then Uncomment The Below Line.
+# wb_name = "OptionChain365"
+
+##############################################################################
+# Note: If All Greeks Icluding 3rd Order Greeks Is Required To Be Streaming
+# In Excel And Terminal, Then Use stream=True, OtherWise To fet 7-9 Second
+# SnapShot of Option Chain use stream=False
+stream = True
+
+##############################################################################
 if __name__ == "__main__":
-    # Note: if all greeks icluding 3rd order greeks is required to be streaming in terminal, use QtsAppRun(_stream=True) at amrket time
-    # QtsAppRun(_stream=True)
-    
-    # Note: If You use office-365 then uncomment the below  line QtsAppRun(_stream=True, ws_name="OptionChain - 365")
-    # QtsAppRun(_stream=True, ws_name="OptionChain - 365")
-    
-    # Note: Else Use QtsAppRun() or QtsAppRun(_stream=False) to get the option chain update din the excel sheet every 7-9 secs.
-    QtsAppRun()
-    
-    # Note: If You use office-365 then uncomment the below  line QtsAppRun(_stream=False, ws_name="OptionChain - 365")
-    # QtsAppRun(ws_name="OptionChain - 365")
+    EnchantQtsApp(
+        stream=stream,
+        _is_running_live=_is_running_live,
+        first_connect=first_connect,
+        wb_name=wb_name,
+    )
