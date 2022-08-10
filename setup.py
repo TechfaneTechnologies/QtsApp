83927a046b35006b78efa9a17c2997c856ba0848
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-08-10 20:19:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='qtsapp',
-      version='0.01',
+      version='0.2',
       description='The python library for QuantsApp',
       url='https://github.com/TechfaneTechnologies/QtsApp',
       author='DrJuneMoone',
